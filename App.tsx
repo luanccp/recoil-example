7357fc8d1a2c3e7e18939eee360934f932786398
Luan Castheloge <luanccp@gmail.com>
2023-08-28 15:59:08 +0100

    chore: setup recoil

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 import {HomeScreen} from './src/screens/Home';
+import {RecoilRoot} from 'recoil';
 
 const App: React.FC = () => {
-  return <HomeScreen />;
+  return (
+    <RecoilRoot>
+      <HomeScreen />
+    </RecoilRoot>
+  );
 };
 
 export default App;
