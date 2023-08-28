58c8c186882aab78c6339a2bbd07ced3a67dea09
Luan Castheloge <luanccp@gmail.com>
2023-08-28 17:42:38 +0100

    feat: adding counter with recoil

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,41 @@
 import React from 'react';
 
-import {Text, View} from 'react-native';
+import {Text, TextInput, View} from 'react-native';
+import {atom, selector, useRecoilState, useRecoilValue} from 'recoil';
+
+const textState = atom({
+  key: 'inputText', // unique ID (with respect to other atoms/selectors)
+  default: '', // default value (aka initial value)
+});
+
+const charCountState = selector({
+  key: 'charCountState', // unique ID (with respect to other atoms/selectors)
+  get: ({get}) => {
+    const text = get(textState);
+
+    return text.length;
+  },
+});
+
+const Counter: React.FC = () => {
+  const [text, setText] = useRecoilState(textState);
+  const count = useRecoilValue(charCountState);
+
+  return (
+    <View>
+      <Text> Characters: {count} </Text>
+      <TextInput
+        value={text}
+        onChangeText={value => setText(value)}
+        placeholder="type here"
+      />
+
+      <View>
+        <Text>Echo: {text}</Text>
+      </View>
+    </View>
+  );
+};
 
 export const HomeScreen: React.FC = () => {
   return (
@@ -11,7 +46,7 @@ export const HomeScreen: React.FC = () => {
         alignItems: 'center',
         backgroundColor: 'pink',
       }}>
-      <Text> Hello world</Text>
+      <Counter />
     </View>
   );
 };
