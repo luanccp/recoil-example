733087fcdfbf04b970c0c143a26f6cd5a6f34d64
Luan Castheloge <luanccp@gmail.com>
2023-10-19 18:03:20 +0100

    feat: setting up tamagui in the wrapper

--- App.tsx
+++ App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import {HomeScreen} from './src/screens/Home';
 import {RecoilRoot} from 'recoil';
+import {TamaguiProvider} from '@tamagui/core';
+import config from './tamagui.config';
 
 const App: React.FC = () => {
   return (
-    <RecoilRoot>
-      <HomeScreen />
-    </RecoilRoot>
+    <TamaguiProvider config={config}>
+      <RecoilRoot>
+        <HomeScreen />
+      </RecoilRoot>
+    </TamaguiProvider>
   );
 };
 
