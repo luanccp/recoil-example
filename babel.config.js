53e1536ee2073d9353ab126fc58ac86fa85cab28
Luan Castheloge <luanccp@gmail.com>
2023-10-19 18:01:01 +0100

    chore: add tamagui

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
+// just to ensure TAMAGUI_TARGET is set
+
+process.env.TAMAGUI_TARGET = 'native';
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: ['transform-inline-environment-variables'],
 };
---
+++ tamagui.config.ts
@@ -0,0 +1,171 @@
+import {createMedia} from '@tamagui/react-native-media-driver';
+
+import {createFont, createTamagui, createTokens} from '@tamagui/core'; // or ''
+const interFont = createFont({
+  family: 'Inter, Helvetica, Arial, sans-serif',
+
+  // keys used for the objects you pass to `size`, `lineHeight`, `weight`
+
+  // and `letterSpacing` should be consistent. The `createFont` function
+
+  // will fill-in any missing values if `lineHeight`, `weight` or `letterSpacing`
+
+  // are subsets of `size`
+
+  size: {
+    1: 12,
+
+    2: 14,
+
+    3: 15,
+  },
+
+  lineHeight: {
+    // 1 will be 22
+
+    2: 22,
+  },
+
+  weight: {
+    1: '300',
+
+    // 2 will be 300
+
+    3: '600',
+  },
+
+  letterSpacing: {
+    1: 0,
+
+    2: -1,
+
+    // 3 will be -1
+  },
+
+  // (native) swap out fonts by face/style
+
+  face: {
+    300: {normal: 'InterLight', italic: 'InterItalic'},
+
+    600: {normal: 'InterBold'},
+  },
+});
+// these keys can be different, but again we highly recommend consistency
+
+const size = {
+  0: 0,
+
+  1: 5,
+
+  2: 10,
+
+  // ....
+};
+export const tokens = createTokens({
+  size: {
+    sm: 38,
+    md: 46,
+    lg: 60,
+  },
+  space: {
+    sm: 15,
+    md: 20,
+    lg: 25,
+  },
+  radius: {
+    sm: 4,
+    md: 8,
+    lg: 12,
+  },
+
+  zIndex: {0: 0, 1: 100, 2: 200},
+
+  color: {
+    white: '#fff',
+
+    black: '#000',
+  },
+});
+
+const config = createTamagui({
+  fonts: {
+    // for tamagui, heading and body are assumed
+
+    heading: interFont,
+
+    body: interFont,
+  },
+
+  tokens,
+
+  themes: {
+    light: {
+      background: '#fff',
+      color: '#000',
+    },
+
+    // define a Button sub-theme, see the Themes docs for more
+    light_Button: {
+      background: '#ccc',
+      backgroundPress: '#bbb', // darker background on press
+      backgroundHover: '#ddd', // lighter background on hover
+      color: '#222',
+    },
+  },
+  // `@tamagui/core` doesn't provide media query capabilities out of the box
+
+  // for native as it is de-coupled from react-native.
+  // For web-only, media queries work out of the box and you can avoid the
+
+  // `createMedia` call here by passing the media object directly.
+  // If targeting React Native, add this driver and use this `createMedia` helper.
+
+  // If web-only you can leave out `createMedia` and just use an object.
+
+  media: createMedia({
+    sm: {maxWidth: 860},
+
+    gtSm: {minWidth: 860 + 1},
+
+    short: {maxHeight: 820},
+
+    hoverNone: {hover: 'none'},
+
+    pointerCoarse: {pointer: 'coarse'},
+  }),
+  // optional:
+  // add custom shorthand props
+
+  // note: as const is important, without it you may see breaking types
+
+  shorthands: {
+    px: 'paddingHorizontal',
+
+    f: 'flex',
+
+    w: 'width',
+  } as const,
+  // Experimental / advanced, only for overriding the core component styles
+
+  // Prefer to use styled() for building your own, only useful for edge cases.
+
+  defaultProps: {
+    Text: {
+      // override any default props here
+    },
+  },
+});
+type AppConfig = typeof config;
+// this will give you types for your components
+
+// note - if using your own design system, put the package name here instead of tamagui
+
+declare module 'tamagui' {
+  interface TamaguiCustomConfig extends AppConfig {}
+  // if you want types for group styling props, define them like so:
+
+  interface TypeOverride {
+    groupNames(): 'a' | 'b' | 'c';
+  }
+}
+export default config;
